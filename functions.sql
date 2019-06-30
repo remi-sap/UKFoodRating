8193772a911fd77237b55e088a14b30a51182c24
remi-sap <remi.astier@sap.com>
2019-06-30 08:56:21 +0200

    Add GIS user creation

--- functions.sql
+++ functions.sql
@@ -1,6 +1,16 @@
---
--- IMPORTANT : You might need to change the schema name
---
+-- create a user for GIS with no password expiration
+CREATE USER GIS password JaimeLe20 no FORCE_FIRST_PASSWORD_CHANGE VALID untIL FOREVER;
+
+--create a schema
+create schema food_rating;
+
+--grant permissions to the user
+grant data admin to GIS;
+grant select,execute,create any on schema food_rating to GIS ;
+
+grant select on "PUBLIC"."ST_SPATIAL_REFERENCE_SYSTEMS"  to GIS ;
+grant select on "PUBLIC"."ST_GEOMETRY_COLUMNS"  to GIS ;
+
 
 create or replace function food_rating.find( NameStartsWith NVARCHAR(64) DEFAULT '')
 /* This function returns all businesses with a location whose name start with the specified string.
